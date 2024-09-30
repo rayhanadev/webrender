8d9d0bfe6fb79f3213beff17abdeb462167dcb26
ray <me@rayhanadev.com>
2024-09-30 02:57:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# WebRender
+
+A simple and easy-to-deploy web service that takes screenshots of
+websites. No extra features or fuss.
+
+Built for Purdue Hackers :yellow_heart:
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import Elysia, { t } from "elysia";
+import puppeteer from "puppeteer";
+
+const app = new Elysia();
+
+app.get("/", (ctx) => {
+  return "Hello World!";
+});
+
+app.get(
+  "/api/render",
+  async (ctx) => {
+    const { url, fullPage } = ctx.query;
+
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const image = await page.screenshot({
+      fullPage: fullPage === "1",
+    });
+
+    await browser.close();
+
+    ctx.set.headers = {
+      "Content-Type": "image/png",
+      "Content-Length": image.length.toString(),
+      "Cache-Control": "max-age=604800",
+    };
+    return image;
+  },
+  { query: t.Object({ url: t.String(), fullPage: t.Optional(t.String()) }) }
+);
+
+app.listen(3000);
